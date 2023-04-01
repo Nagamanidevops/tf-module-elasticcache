6147f17c13b6047e4c93b295eb8e3ecfbe486aee
nagamani <nagamanichavaaws@gmail.com>
2023-04-01 12:09:09 +0000

    node

--- vars.tf
+++ vars.tf
@@ -3,6 +3,6 @@ variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
 variable  "num_cache_nodes" {}
-variable "num_node_groups"  {}
+variable "node_type"  {}
 #variable  "replicas_per_node_group" {}
 variable "engine_version" {}
